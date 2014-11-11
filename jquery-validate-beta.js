552d75547a1a81e44a8744e5bb5ccbf75cd206d6
janking <315359131@qq.com>
2014-11-11 18:19:40 +0800

    company committed

--- jquery-validate-beta.js
+++ jquery-validate-beta.js
@@ -8,8 +8,12 @@
             err: '邮箱格式不正确'
         },
         zh: {
-            reg: /[\u2E80-\u2EFF\u2F00-\u2FDF\u3000-\u303F\u31C0-\u31EF\u3200-\u32FF\u3300-\u33FF\u3400-\u4DBF\u4DC0-\u4DFF\u4E00-\u9FBF\uF900-\uFAFF\uFE30-\uFE4F\uFF00-\uFFEF]+/g,
+            reg: /^[\u2E80-\u2EFF\u2F00-\u2FDF\u3000-\u303F\u31C0-\u31EF\u3200-\u32FF\u3300-\u33FF\u3400-\u4DBF\u4DC0-\u4DFF\u4E00-\u9FBF\uF900-\uFAFF\uFE30-\uFE4F\uFF00-\uFFEF]+$/g,
             err: '必须为中文'
+        },
+        num:{
+            reg:/^\d+$/g,
+            err:"必须为数字"
         },
         mobile: {
             reg: /^1[3|4|5|8][0-9]\d{8}$/,
