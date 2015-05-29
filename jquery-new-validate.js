425fe6201ade7fce202f2edeb439078f815218eb
janking <315359131@qq.com>
2015-05-29 20:53:18 +0800

    new validate

---
+++ jquery-new-validate.js
@@ -0,0 +1,207 @@
+(function(factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery, window, document);
+    }
+}(function($, window, document, undefined) {
+
+
+    /*************************策略对象*****************************/
+
+    var RULES = {
+        isNonEmpty: function(value, errorMsg) {
+            if (value.length === 0) {
+                return errorMsg;
+            }
+        },
+        minLength: function(value, length, errorMsg) {
+            if (value.length < length) {
+                return errorMsg;
+            }
+        },
+        isMobile: function(value, errorMsg) {
+            if (!/(^1[3|5|8][0-9]{9}$)/.test(value)) {
+                return errorMsg;
+            }
+        },
+        between: function(value, range, errorMsg) {
+            var min = parseInt(range.split('-')[0]);
+            var max = parseInt(range.split('-')[1]);
+            if (value.length < min || value.length > max) {
+                return errorMsg;
+            }
+        },
+        onlyNum: function(value, errorMsg) {
+            if (!/^[0-9]*$/.test(value)) {
+                return errorMsg;
+            }
+        }
+    };
+
+    /*************************Validator类*****************************/
+
+
+    var Validator = function() {
+        this.cache = [];
+    };
+
+    Validator.prototype.add = function(dom, rules) {
+        var self = this;
+        for (var i = 0, rule; rule = rules[i++];) {
+            (function(rule) {
+                var strategyAry = rule.strategy.split(':');
+                var errorMsg = rule.errorMsg
+                self.cache.push(function() {
+                    var strategy = strategyAry.shift(); // 前删匹配方式并赋值
+                    strategyAry.unshift(dom.value); // 前插value值
+                    strategyAry.push(errorMsg); // 后插出错提示
+                    return {
+                        errorMsg: RULES[strategy].apply(dom, strategyAry),
+                        el: dom
+                    };
+                });
+            }(rule));
+        }
+    };
+
+    Validator.prototype.start = function() {
+        for (var i = 0, validatorFunc; validatorFunc = this.cache[i++];) {
+            var errorMsg = validatorFunc();
+            if (errorMsg) {
+                return errorMsg;
+            }
+        };
+    };
+
+    Validator.prototype.showMsg = function(target, msg, status, callback) {
+        //status
+        // 0 : tip
+        // 1 : success
+        // 2 : error
+        var _current = status ? (status > 1 ? 'error' : 'success') : 'tip';
+        var $context = target.parent();
+        var $msg = $context.find('.valid_message');
+        $msg.remove();
+        target.removeClass('success error').addClass(_current);
+        $context.append('<span class="valid_message ' + _current + '">' + msg + '</span>');
+    };
+
+    var plugin = {
+        init: function(options) {
+            var $form = this;
+            var $body = $('body');
+            var $required = $form.find('.required');
+            var setting = $.extend({
+                onBlur: null,
+                onFocus: null,
+                onChange: null
+            }, options);
+
+            if (options.type) {
+                $.extend(RULES, options.type);
+            }
+
+            var validator = new Validator();
+
+            $body.on({
+                focus: function(event) {
+                    var $this = $(this);
+                    var _tipMsg = $this.attr('data-tip') || '';
+                    validator.showMsg($this, _tipMsg);
+                    setting.onFocus ? setting.onFocus.call($this,arguments) : '';
+                },
+                blur: function(event) {
+                    var $this = $(this);
+                    var dataValid = $this.attr('data-valid');
+                    var validLen = dataValid.split('||');
+                    var errCollection = $this.attr('data-error');
+                    var errMsgAry = errCollection.split("||");
+                    var strategyAry, strategy, errMsg;
+
+                    for (var i = 0; i < validLen.length; i++) {
+                        strategyAry = validLen[i].split(':');
+                        strategy = strategyAry.shift();
+                        strategyAry.unshift(this.value);
+                        strategyAry.push(errMsgAry[i]);
+                        errMsg = RULES[strategy].apply(this, strategyAry);
+                        if (errMsg) {
+                            $this.attr('data-status', 0);
+                            validator.showMsg($this, errMsg, 2);
+                            return false;
+                        }
+                    };
+
+                    if (!errMsg) {
+                        $this.attr('data-status', 1).removeClass('error');
+                        $this.parent().find('.valid_message').remove();
+                    }
+
+                    setting.onBlur ? setting.onBlur.call($this,argumemts) : '';
+                },
+                change: function(event) {
+                    setting.onChange ? setting.onChange.call($this,arguments) : '';
+                }
+            }, '.required');
+
+
+        },
+        submitValidate: function(options) {
+            var $form = options || this;
+            var $body = $('body');
+            var $required = $form.find('.required');
+            var validator = new Validator();
+            var $target;
+
+            $.each($required, function(index, el) {
+                var $el = $(el);
+                var dataValid = $el.attr('data-valid');
+                var validLen = dataValid.split('||');
+                var errCollection = $el.attr('data-error');
+                var errMsgAry = errCollection.split("||");
+                var ruleAry = [];
+
+                for (var i = 0; i < validLen.length; i++) {
+                    ruleAry.push({
+                        strategy: validLen[i],
+                        errorMsg: errMsgAry[i]
+                    });
+                };
+
+                validator.add(el, ruleAry);
+
+            });
+
+            var result = validator.start();
+
+            if (result.errorMsg) {
+                $target = $(result.el);
+                 $target.attr('data-status',0)[0].focus();
+                 validator.showMsg($target,result.errorMsg, 2);
+                 return false;
+            }
+
+            return true;
+        }
+    };
+
+    $.fn.validate = function() {
+        var method = arguments[0];
+        if (plugin[method]) {
+            method = plugin[method];
+            arguments = Array.prototype.slice.call(arguments, 1);
+        } else if (typeof(method) == 'object' || !method) {
+            method = plugin.init;
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.validate Plugin');
+            return this;
+        }
+        return method.apply(this, arguments);
+    }
+
+}))
